7201e1c76d2390a629065165dd4711c172a0c75b
Bowie <marketing@letscode.com.br>
2023-06-09 13:41:24 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/evandroms89.png" />
-    <Titulo fontSize={20}>Evandro Moraes</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={16}>Evandro Moraes</Titulo>
+      <Paragrafo tipo="secundario" fontSize={14}>
+        evandroms89
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
